141842c667b090bc899346c0d5186d5b22285a81
ilix <alex@desolace.se>
2014-07-25 15:11:00 +0200

    Added sftp deploy task.

--- Gruntfile.js
+++ Gruntfile.js
@@ -162,6 +162,22 @@ module.exports = function (grunt) {
           run: true
         }
       }
+    },
+    secret: grunt.file.readJSON('secret.json'),
+    sftp: {
+      stage: {
+        files: {
+          "./": "dist/**"
+        },
+        options: {
+          path: '/var/www/ingr',
+          host: '<%= secret.host %>',
+          username: '<%= secret.username %>',
+          password: '<%= secret.password %>',
+          srcBasePath: 'dist/',
+          showProgress: true
+        }
+      }
     }
   });
 
@@ -180,9 +196,11 @@ module.exports = function (grunt) {
   grunt.loadNpmTasks('grunt-ngmin');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-mocha');
+  grunt.loadNpmTasks('grunt-ssh');
 
   grunt.registerTask('build',['clean:before','less','dom_munger:readcss','dom_munger:readscripts','ngtemplates','cssmin','concat','ngmin','uglify','copy','dom_munger:removecss','dom_munger:addcss','dom_munger:removescripts','dom_munger:addscript','htmlmin','imagemin','clean:after']);
   grunt.registerTask('test',['jshint', 'mocha']);
   grunt.registerTask('server', ['connect']);
+  grunt.registerTask('deploy', ['build', 'sftp']);
   grunt.registerTask('default', ['test', 'server', 'watch']);
 };
