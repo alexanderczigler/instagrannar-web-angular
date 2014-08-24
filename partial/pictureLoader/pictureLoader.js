82d4b6530d68999561d2ed6cb26894f2be8cacd2
ilix <alex@desolace.se>
2014-08-24 08:59:56 +0200

    Restyled the load button and changed its caption. Also added a function that disables it for two seconds after each click.

--- partial/pictureLoader/pictureLoader.js
+++ partial/pictureLoader/pictureLoader.js
@@ -1,14 +1,22 @@
-angular.module('ingr-web').controller('PictureLoaderCtrl', function ($scope, $rootScope, mapsHelper) {
+angular.module('ingr-web').controller('PictureLoaderCtrl', function ($scope, $rootScope, mapsHelper, $timeout) {
   'use strict';
 
   $scope.place = $rootScope.place;
+  $scope.buttonDisabled = false;
 
   $scope.load = function(zoom) {
-    console.log('click', zoom);
+    $scope.brieflyDisableLoadButton();
     $rootScope.safeApply(function () {
       $scope.place.dst = mapsHelper.mapZoomToDistance(zoom);
       $rootScope.place.changed = true;
     });
+  };
+
+  $scope.brieflyDisableLoadButton = function() {
+    $scope.buttonDisabled = true;
+    $timeout(function() {
+      $scope.buttonDisabled = false;
+    }, 2000);
   };
 
 });
