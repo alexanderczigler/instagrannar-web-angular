114348d4cc4011b9f9ebf08f0a70b3ed642278d0
ilix <code@ilix.se>
2014-10-09 18:57:48 +0200

    Changed default location to kaknästornet.

--- service/viewport.js
+++ service/viewport.js
@@ -2,8 +2,8 @@ angular.module('ingr-web').service('viewport', function () {
   'use strict';
 
   var viewport = {
-    latitude: 59.34199,
-    longitude: 18.051995,
+    latitude: 59.335004,
+    longitude: 18.126814,
     zoomLevel: 14
   };
 
