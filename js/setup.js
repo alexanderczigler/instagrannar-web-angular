b0c463172fb4d26692da98f0add8794992264a68
ilix <code@ilix.se>
2014-10-15 19:19:33 +0200

    What?

--- js/setup.js
+++ js/setup.js
@@ -3,8 +3,14 @@ angular.module('ingr-web', ['ui.router', 'ngResource', 'google-maps']);
 angular.module('ingr-web').config(function ($stateProvider, $urlRouterProvider) {
   'use strict';
 
-  $stateProvider.state('annonsering', {    url: '/annonsering',    templateUrl: 'partial/annonsering/annonsering.html'  });
-	$stateProvider.state('legal', {    url: '/legal',    templateUrl: 'partial/legal/legal.html'  });
+  $stateProvider.state('annonsering', {
+    url: '/annonsering',
+    templateUrl: 'partial/annonsering/annonsering.html'
+  });
+	$stateProvider.state('legal', {
+    url: '/legal',
+    templateUrl: 'partial/legal/legal.html'
+  });
 	/* Add New Routes Above */
   
   // For any unmatched url, redirect to /
@@ -12,14 +18,14 @@ angular.module('ingr-web').config(function ($stateProvider, $urlRouterProvider) 
 
 });
 
-angular.module('ingr-web').run(function ($rootScope, location) {
+angular.module('ingr-web').run(function ($rootScope, $timeout, location) {
   'use strict';
 
   $rootScope.loadPictures = false;
   $rootScope.selectedInstagram = {};
 
   location.setViewPort();
-
+  
   $rootScope.safeApply = function (fn) {
     var phase = $rootScope.$$phase;
     if (phase === '$apply' || phase === '$digest') {
