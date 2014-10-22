461b22ef36c7df759d0fc8c2278e88ef19b93e20
ilix <code@ilix.se>
2014-10-22 22:55:55 +0200

    Changed photoView to use images service.

--- partial/photoView/photoView.js
+++ partial/photoView/photoView.js
@@ -1,21 +1,21 @@
-angular.module('ingr-web').controller('PhotoViewCtrl', function ($scope, $rootScope) {
+angular.module('ingr-web').controller('PhotoViewCtrl', function ($scope, $rootScope, $stateParams, images) {
   'use strict';
 
   $scope.instagram = {};
+
+  if (!!$stateParams.id) {
+    console.log('a', images.data);
+    images.data.data.map(function (i) {
+      if (i.id === $stateParams.id) {
+        $scope.instagram = i;
+      }
+    });
+  }
 
   $scope.t = function (time) {
     var day = moment.unix(time);
     var m = moment(day, 'YYYYMMDD').fromNow();
     return m;
   };
-
-  $rootScope.$watch('selectedInstagram', function(si) {
-    if (!si) {
-      return;
-    }
-  	if (si.link) {
-			$scope.instagram = $rootScope.selectedInstagram;
-  	}
-  });
 
 });
